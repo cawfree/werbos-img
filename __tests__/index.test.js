2d0f983f44f231227212a3557c901d9d360da39e
cawfree <hello@cawfree.com>
2020-02-20 16:23:39 +0000

    Verified support for shuffling.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import "@babel/polyfill";
 import "@tensorflow/tfjs-node";
 
-import werbos, { threshold } from "@werbos/core";
+import werbos, { threshold, oneHot, shuffle } from "@werbos/core";
 import { print, noop } from "rippleware";
 
 import { img } from "../src";
@@ -17,16 +17,19 @@ it("should be capable of reading image files", async () => {
   // TODO: Need to throw on invalid aspect ratio
   const withLabels = () => h => h('[*]', input => [
     [].concat(...input),
-    [].concat(...[].concat(...input.map((e, i) => [...Array(e.length)].fill(i)))),
+    [].concat(...input.map((e, i) => [...Array(e.length)].fill([i]))),
   ]);
+
+  const printer = () => h => h(t => t.print());
     
   const app = werbos()
     .use(img({ width: 28, height: 28 }))
     .use(withLabels())
-    .use(threshold(), noop())
-    .use(print(), noop());
+    .use(threshold(), oneHot())
+    .use(shuffle());
   
   const a = new Date().getTime();
+
   // https://github.com/myleott/mnist_png
   const result = await app(
     [
@@ -42,11 +45,6 @@ it("should be capable of reading image files", async () => {
       '/home/cawfree/Downloads/tmp/mnist_png/training/9',
     ],
   );
-
-  // time to beat (untensored)
-  // 150.061 s
-  console.log('built in ts:');
-  console.log(((new Date().getTime()) - a) / 1000, 's');
 
   expect(true)
     .toBeTruthy();
