b01dbfbb6fbcb092bd9e5b1e1830064479caf0a2
cawfree <hello@cawfree.com>
2020-02-20 01:18:22 +0000

    Implemented thresholding using sharp.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,20 +3,46 @@ import "@babel/polyfill";
 import compose from "rippleware";
 import { img } from "../src";
 
+jest.setTimeout(24* 60 * 60 * 1000);
+
 it("should be capable of reading image files", async () => {
 
   const app = compose()
     .use(img());
-
-  const result = await app('/home/cawfree/Downloads/unnamed.jpg');
-  const result2 = await app(['/home/cawfree/Downloads/unnamed.jpg']);
-
-  console.log(result, result2);
-
-  expect(app())
-    .rejects
-    .toBeTruthy();
   
+  const result = await app(
+    [
+      '/home/cawfree/Downloads/tmp/mnist_png/training/0',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/1',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/2',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/3',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/4',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/5',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/6',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/7',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/8',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/9',
+    ],
+  );
+
+  result.forEach(
+    (e) => {
+      console.log(e.length);
+    },
+  );
+
+  //console.log(result);
+
+  //await app([
+  //  '/home/cawfree/Downloads/squiggly.png',
+  //  '/home/cawfree/Downloads/squiggly.png',
+  //  '/home/cawfree/Downloads/squiggly.png',
+  //]);
+
+  //expect(app())
+  //  .rejects
+  //  .toBeTruthy();
+  //
   expect(true)
     .toBeTruthy();
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,86 @@
+import sharp from "sharp";
 import { typeCheck } from "type-check";
-import { existsSync } from "fs";
+import { existsSync, lstatSync, readdirSync } from "fs";
+import { sep } from "path";
+
+const defaultOptions = Object
+  .freeze({
+    width: 64,
+    height: 64,
+  });
+
+const isDirectory = e => typeCheck('String', e) && lstatSync(e).isDirectory();
+const isDirectories = e => Array.isArray(e) && e.length > 0 && e.reduce((r, f) => r && isDirectory(f), true);
 
 const isFile = e => typeCheck('String', e) && existsSync(e);
 
 const isFiles = e => Array.isArray(e) && e.length > 0 && e.reduce((r, f) => (r && isFile(f)), true);
 
-export const img = () => handle => [
-  handle(isFile, () => 'file!'),
-  handle(isFiles, () => 'files!'),
-] && undefined;
+const ensureAspectRatio = (images) => {
+  if (images.length <= 0) {
+    throw new Error('Expected at least a single image, but encountered none.');
+  }
+  const ars = images
+    .map(({ bitmap: { width, height } }) => (width / height));
+  if ([...new Set(ars)].length !== 1) {
+    throw new Error("You have specified an image range consisting of multiple different aspect ratios.");
+  }
+  return images;
+};
+
+const openFiles = (opts, input, hooks) => {
+  const { width, height } = opts;
+  // TODO: need to enforce that all images have the same aspect ratio
+  return input
+    .reduce(
+      (p, path, i) => p
+        .then(
+          (images) => Promise
+            .resolve(
+              sharp(path)
+                .resize(width, height)
+                .threshold()
+            )
+            .then((scaledImage) => (images.push(scaledImage) && undefined) || images),
+        ),
+      Promise.resolve([]),
+    );
+};
+
+const openDirectories = (opts, inputs, hooks) => Promise
+  .all(
+    inputs.map(
+      dir => Promise.resolve(readdirSync(dir))
+        .then(files => openFiles(
+          opts,
+          files.map(file => `${dir}${sep}${file}`),
+          hooks,
+        )),
+    ),
+  );
+  
+//  input
+//  .reduce(
+//    (p, dir) => p
+//      .then(
+//        imgs => Promise.resolve(readdirSync(dir))
+//          .then(files => openFiles(
+//            opts,
+//            files.map(file => `${dir}${sep}${file}`),
+//            hooks,
+//          ))
+//          .then(extras => imgs.concat(...extras)),
+//      ),
+//    Promise.resolve([]),
+//  );
+
+// TODO: Register using global state!
+export const img = (options = defaultOptions) => (handle) => {
+  const opts = { ...defaultOptions, ...options };
+  return [
+    handle(isDirectory, (input, hooks) => openDirectories(opts, [input], hooks)),
+    handle(isDirectories, (input, hooks) => openDirectories(opts, input, hooks)),
+    handle(isFile, (input, hooks) => openFiles(opts, [input], hooks)),
+    handle(isFiles, (input, hooks) => openFiles(opts, input, hooks)),
+  ] && undefined;
+};
