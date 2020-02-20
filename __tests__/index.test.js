31906b86e64f5ad597975d262173428b819dcd29
cawfree <hello@cawfree.com>
2020-02-20 01:34:32 +0000

    Verified separation.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,27 @@
 import "@babel/polyfill";
 
-import compose from "rippleware";
+import compose, { print, noop } from "rippleware";
+
 import { img } from "../src";
 
 jest.setTimeout(24* 60 * 60 * 1000);
 
 it("should be capable of reading image files", async () => {
 
+  // TODO: Need to understand the appropriate style for image shape
+  //       try to create something homogeneous which would effectively
+  //       be compatible with jimp and sharp
+
+  // TODO: Need to throw on invalid aspect ratio
+  const applyLabels = () => h => h('[*]', input => [
+    [].concat(...input),
+    [].concat(...[].concat(...input.map((e, i) => [...Array(e.length)].fill(i)))),
+  ]);
+    
   const app = compose()
-    .use(img());
+    .use(img())
+    .use(applyLabels())
+    .use(noop(), print());
   
   const result = await app(
     [
