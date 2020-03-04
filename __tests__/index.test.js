74ecb3c7a0ce23cb4d553c54565df6814051d38b
cawfree <hello@cawfree.com>
2020-03-04 00:02:08 +0000

    Implemented meta propagation.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 import "@babel/polyfill";
 import "@tensorflow/tfjs-node";
 
+import { typeCheck } from "type-check";
 import werbos, { threshold, oneHot, shuffle, sequential, dense, train } from "@werbos/core";
 import { justOnce } from "rippleware";
 
@@ -10,26 +11,38 @@ jest.setTimeout(24* 60 * 60 * 1000);
 
 it("should be capable of reading image files", async () => {
 
-  //const app = werbos()
-  //  .use(img({ width: 28, height: 28 }));
-
-  //await app('/home/cawfree/Downloads/logo.png');
-
-  // TODO: Need to understand the appropriate style for image shape
+    // TODO: Need to understand the appropriate style for image shape
   //       try to create something homogeneous which would effectively
   //       be compatible with jimp and sharp
 
-  // TODO: Need to throw on invalid aspect ratio
-  const withLabels = () => [
-    ['[*]', input => [
-      [].concat(...input),
-      [].concat(...input.map((e, i) => [...Array(e.length)].fill([i]))),
-    ]],
-  ];
+  //// TODO: Need to throw on invalid aspect ratio
+  //const withLabels = () => [
+  //  ['[*]', input => [
+  //    [].concat(...input),
+  //    [].concat(...input.map((e, i) => [...Array(e.length)].fill([i]))),
+  //  ]],
+  //];
 
   const app = werbos()
     .use(img({ width: 28, height: 28 }))
-    .sep(withLabels())
+    .use(
+      [
+        ['[Buffer]', (e) => console.log('dude, got here')],
+      ],
+    )
+    .use(
+      (_, { useMeta }) => console.log(JSON.stringify(useMeta())),
+    );
+
+  await app(
+    [
+      //'/home/cawfree/Downloads/tmp/mnist_png/training/0',
+      //'/home/cawfree/Downloads/tmp/mnist_png/training/1',
+      '/home/cawfree/Downloads/tmp/mnist_png/training/1/1230.png',
+    ],
+  );
+    
+    //.sep(withLabels())
     //.mix(threshold(), oneHot())
     //.use(justOnce(shuffle()))
     //.use(
@@ -39,23 +52,75 @@ it("should be capable of reading image files", async () => {
     //)
     //.use(train({ epochs: 5, batchSize: 28 }));
   
-  // https://github.com/myleott/mnist_png
-  const result = await app(
-    [
-      '/home/cawfree/Downloads/tmp/mnist_png/training/0',
-      '/home/cawfree/Downloads/tmp/mnist_png/training/1',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/2',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/3',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/4',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/5',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/6',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/7',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/8',
-      //'/home/cawfree/Downloads/tmp/mnist_png/training/9',
-    ],
-  );
+  //// https://github.com/myleott/mnist_png
+  //const result = await app(
+  //  [
+  //    '/home/cawfree/Downloads/tmp/mnist_png/training/0',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/1',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/2',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/3',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/4',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/5',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/6',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/7',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/8',
+  //    //'/home/cawfree/Downloads/tmp/mnist_png/training/9',
+  //  ],
+  //);
 
-  console.log(result);
+  //const first = result[0][0];
+
+  //var opt = {
+  //  customTypes: {
+  //    Buffer: {
+  //      typeOf: 'Uint8Array',
+  //      validate: Buffer.isBuffer,
+  //    },
+  //  },
+  //};
+
+  //console.log(typeCheck('Buffer', first, opt));
+
+  //console.log(first);
+
+  //var opt = {
+  //  customTypes: {
+  //    Even: {
+  //      typeOf: 'Object',
+  //      //typeOf: 'Number',
+  //      validate: function(x) {
+  //        console.log('validating',x);
+  //        return x % 2 === 0;
+  //      },
+  //    },
+  //  },
+  //};
+
+  //typeCheck('Even', first, opt);
+
+  //console.log(
+  //  typeCheck(
+  //    'Buffer',
+  //    first,
+  //    {
+  //      customTypes: {
+  //        Buffer: {
+  //          typeOf: 'NaN',
+  //          validate: (x) => {
+  //            console.log('should validate',x);
+  //            return false;
+  //          },//Buffer.isBuffer(x),
+  //        },
+  //      },
+  //    },
+  //  ),
+  //);
+
+  //console.log(Object.keys(first));
+  //console.log(typeCheck("Array", first));
+  //console.log(typeCheck("*", first));
+  //console.log(typeCheck("Buffer", first));
+  //console.log(typeCheck("Object", first));
 
   //const result2 = await app(
   //  [
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { typeCheck } from "type-check";
 import { existsSync, lstatSync, readdirSync } from "fs";
 import { sep } from "path";
 
+const imageInfoShape = '{channels:Number,width:Number,height:Number}';
+
 const defaultOptions = Object
   .freeze(
     {
@@ -10,6 +12,14 @@ const defaultOptions = Object
       height: null,
     },
   );
+
+const throwOnImageInfoMismatch = (nextInfo, lastInfo) => {
+  const { channels: nextChannels } = nextInfo;
+  const { channels: lastChannels } = lastInfo;
+  if (nextChannels !== lastChannels) {
+    throw new Error(`The number of input channels for image data must be homogeneous.`);
+  }
+};
 
 const isDirectory = e => typeCheck('String', e) && lstatSync(e).isDirectory();
 const isDirectories = e => Array.isArray(e) && e.length > 0 && e.reduce((r, f) => r && isDirectory(f), true);
@@ -30,19 +40,22 @@ const toBuffer = img => new Promise(
     ),
 );
 
-const openFiles = (opts, input, hooks) => {
-  const { useMeta } = hooks;
+const openFiles = (opts, input) => {
   const { width, height } = opts;
   if (!Number.isInteger(width) || width <= 0) {
     return Promise.reject(new Error(`Expected positive integer width, but encountered ${width}.`));
   } else if (!Number.isInteger(height) || height <= 0) {
     return Promise.reject(new Error(`Expected positive integer height, but encountered ${height}.`));
   }
+  // TODO: It would be nicer to allow external callers to take advantage of sharp
+  //       (or some generic sharp/jimp interface) to have finer control over these
+  //       processes. You know, depending on the call context, maybe this looks
+  //       like a composable instance. Maybe not.
   return input
     .reduce(
       (p, path, i) => p
         .then(
-          (images) => Promise
+          ([images, lastInfo]) => Promise
             .resolve(
               sharp(path)
                 .resize(width, height)
@@ -50,35 +63,100 @@ const openFiles = (opts, input, hooks) => {
                 .raw(),
             )
             .then(toBuffer)
-            .then(([buf]) => buf),
-            // implicitly converts to data; let's use  the buffer
-            //.then(([buf]) => (images.push([...buf]) && undefined) || images)
-            //// TODO: This channel information is potentially incorrect dependent upon the input data.
-            //.then(data => useMeta({ ...useMeta(), [Meta.Transform]: { channels: 3 } }) || data),
+            .then(
+              ([nextBuffer, nextInfo]) => {
+                // XXX: Basically enforces the same channel.
+                (i > 0) && throwOnImageInfoMismatch(
+                  nextInfo,
+                  lastInfo,
+                );
+                return [
+                  isNaN(images.push(nextBuffer)) || images,
+                  nextInfo,
+                ];
+              },
+            ),
         ),
-      Promise.resolve([]),
+      Promise.resolve([[], undefined]),
+    )
+    .then(
+      // XXX: At this point, we can guarantee the same number of channels,
+      //      width and height for all processed image data. We'll need to
+      //      apply additional checks for anything more complicated than this.
+      ([images, { channels, width, height }]) => [
+        images,
+        {
+          channels,
+          width,
+          height,
+        },
+      ],
     );
 };
 
-const openDirectories = (opts, inputs, hooks) => Promise
+const openDirectories = (opts, inputs) => Promise
   .all(
     inputs.map(
       dir => Promise.resolve(readdirSync(dir))
         .then(files => openFiles(
           opts,
           files.map(file => `${dir}${sep}${file}`),
-          hooks,
         )),
     ),
   );
+
+const persistChannelMeta = (useMeta, width, height, channels) => {
+  if (!Number.isInteger(width) || width <= 0) {
+    throw new Error(`Expected positive integer width, encountered ${width}.`);
+  } else if (!Number.isInteger(height) || height <= 0) {
+    throw new Error(`Expected positive integer height, encountered ${height}.`);
+  } else if (!Number.isInteger(channels) || channels <= 0) {
+    throw new Error(`Expected positive integer channels, encountered ${channels}.`);
+  }
+  return useMeta(
+    {
+      ...useMeta(),
+      ['somewhere']: {
+        width,
+        height,
+        channels,
+      },
+    },
+  );
+};
+
+// XXX: Returns the appropriate result data alongside
+//      the required meta for buffer data.
+const forwardResults = (results, { useMeta, useMatcher }) => {
+  const typeCheck = useMatcher();
+  if (typeCheck(`([Buffer],${imageInfoShape})`, results)) {
+    const [bufs, { width, height, channels }] = results;
+    return persistChannelMeta(useMeta, width, height, channels) || bufs;
+  } else if (`[([Buffer], ${imageInfoShape})]`, results) {
+    const { length } = results;
+    for (let i = 1; i < length; i += 1) {
+      throwOnImageInfoMismatch(results[i], results[i - 1]);
+    }
+    // XXX: At this stage, we basically know all of these quantities are the same.
+    const [[_, { width, height, channels }]] = results;
+    const bufs = results
+      .map(([buf]) => buf);
+    return persistChannelMeta(useMeta, width, height, channels) || bufs;
+  }
+  throw new Error(`💥 It looks you might be comparing collections of images against a singular image source, and it is not possible to create tensors based on such a combination. If you believe this may be a bug, please file an issue at https://github.com/cawfree/werbos-img/issues.`);
+};
   
 // TODO: Register using global state!
 export const img = (options = defaultOptions) => {
   const opts = { ...defaultOptions, ...options };
   return [
-    [isDirectory, (input, hooks) => openDirectories(opts, [input], hooks)],
-    [isDirectories, (input, hooks) => openDirectories(opts, input, hooks)],
-    [isFile, (input, hooks) => openFiles(opts, [input], hooks)],
-    [isFiles, (input, hooks) => openFiles(opts, input, hooks)],
+    [isDirectory, (input, hooks) => openDirectories(opts, [input])
+      .then(results => forwardResults(results, hooks))],
+    [isDirectories, (input, hooks) => openDirectories(opts, input)
+      .then(results => forwardResults(results, hooks))],
+    [isFile, (input, hooks) => openFiles(opts, [input])
+      .then(results => forwardResults(results, hooks))],
+    [isFiles, (input, hooks) => openFiles(opts, input)
+      .then(results => forwardResults(results, hooks))],
   ];
 };
